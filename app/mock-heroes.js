4f30c698c130e606bdc280dd5a4133257a2d9dbe
Bill <wkernan@gmail.com>
2016-10-17 10:43:38 -0500

    angular2 tut commit

---
+++ app/mock-heroes.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Ken' },
+    { id: 12, name: 'Chun LI' },
+    { id: 13, name: 'Blanca' },
+    { id: 14, name: 'Zangeef' },
+    { id: 15, name: 'E. Honda' },
+    { id: 16, name: 'M. Bison' },
+    { id: 17, name: 'Guile' },
+    { id: 18, name: 'Tony' }
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Ken' },
+	{ id: 12, name: 'Chun LI' },
+	{ id: 13, name: 'Blanca'},
+	{ id: 14, name: 'Zangeef' },
+	{ id: 15, name: 'E. Honda' },
+	{ id: 16, name: 'M. Bison' },
+	{ id: 17, name: 'Guile' },
+	{ id: 18, name: 'Tony' }
+];
